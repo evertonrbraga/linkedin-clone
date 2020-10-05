d08f89285c5d51cbfc5e595f050a046b191d7973
Everton Braga <evertonr.braga@gmail.com>
2020-10-05 13:42:53 -0300

    Main section components first steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import GlobalStyles from './styles/GlobalStyles';
+import Layout from "./components/Layout";
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
   return (
     <>
-      <h1>Rocketseat</h1>
-
+      <Layout />
       <GlobalStyles />
     </>
   );
