5fbace17a801a4adc81cb95a5789b20d3dfa6740
Everton Braga <evertonr.braga@gmail.com>
2020-10-07 19:57:08 -0300

    Desktop header finished

--- src/components/DesktopHeader/styles.ts
+++ src/components/DesktopHeader/styles.ts
@@ -1,6 +1,6 @@
-import styled, { css } from "styled-components";
-import { GrLinkedin } from "react-icons/gr";
-import { AiFillHome, AiOutlineBell, AiFillCaretDown } from "react-icons/ai";
+import styled, { css } from 'styled-components';
+import { GrLinkedin } from 'react-icons/gr';
+import { AiFillHome, AiOutlineBell, AiFillCaretDown } from 'react-icons/ai';
 
 export const Container = styled.header`
   background: var(--color-header);
@@ -19,11 +19,74 @@ export const Container = styled.header`
   }
 `;
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex: 1;
 
-export const LinkedInIcon = styled(GrLinkedin)``;
+  max-width: 1180px;
+  margin: 0 auto;
+  height: 52px;
 
-export const SearchInput = styled.div``;
+  .left,
+  .right nav {
+    display: flex;
+    align-items: center;
+  }
+
+  .right nav {
+    height: 100%;
+
+    button {
+      background: none;
+      border: 0;
+      outline: 0;
+
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      min-width: 90px;
+      min-height: 100%;
+
+      color: var(--color-icons);
+      cursor: pointer;
+
+      &:hover {
+        color: var(--color-white);
+      }
+
+      &.active {
+        border-bottom: 2px solid var(--color-white);
+      }
+    }
+  }
+`;
+
+export const LinkedInIcon = styled(GrLinkedin)`
+  height: 34px;
+  width: 34px;
+  color: var(--color-linkedin);
+  background: #fff;
+  border-radius: 4px;
+  flex-shrink: 0;
+`;
+
+export const SearchInput = styled.input`
+  margin-left: 12px;
+
+  background: var(--color-input);
+  color: var(--color-black);
+  font-size: 14px;
+  padding: 7.5px 8px;
+  border: none;
+  outline: none;
+  border-radius: 2px;
+
+  &:focus {
+    background: var(--color-white);
+  }
+`;
 
 const generalIconCSS = css`
   width: 24px;
@@ -46,5 +109,6 @@ export const ProfileCircle = styled.img`
 `;
 
 export const CaretDownIcon = styled(AiFillCaretDown)`
-  ${generalIconCSS}
+  width: 16px;
+  height: 16px;
 `;
