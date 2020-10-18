3c834435155961c74f79aaa74d9762a7d6d2a3f2
Everton Braga <evertonr.braga@gmail.com>
2020-10-18 00:14:11 -0300

    Starting middle column structure

--- src/components/MiddleColumn/index.tsx
+++ src/components/MiddleColumn/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import LoadingFeedPost from '../Shimmer/LoadingFeedPost';
 import FeedShare from './FeedShare';
 import FeedPost from './FeedPost';
 
@@ -8,11 +9,23 @@ import { Container } from './styles';
 const MiddleColumn: React.FC<LoadingProps> = ({ isLoading }) => {
   return (
     <Container className='middle-column'>
-      <FeedShare />
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
+      {isLoading ? (
+        <>
+          {/* <LoadingFeedShare /> */}
+          <LoadingFeedPost />
+          <LoadingFeedPost />
+          <LoadingFeedPost />
+          <LoadingFeedPost />
+        </>
+      ) : (
+        <>
+          <FeedShare />
+          <FeedPost />
+          <FeedPost />
+          <FeedPost />
+          <FeedPost />
+        </>
+      )}
     </Container>
   );
 };
